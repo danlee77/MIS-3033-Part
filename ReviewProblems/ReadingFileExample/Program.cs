80cc702c0c1c6b5194479d0e2f648eb9ed0c05b8
Daniel Lee <Daniel.Lee-1@ou.edu>
2020-09-03 17:42:18 -0500

    second commit

---
+++ ReviewProblems/ReadingFileExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFileExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Daniel\Desktop\Pride and Prejudice by Jane Austen.txt";
+
+            //string entireBook = File.ReadAllText(filePath);
+            string[] allLines = File.ReadAllLines(filePath);
+
+            /*
+            //Output 5 lines of the book at a time
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                if (i % 5 == 0 && i != 0)
+                {
+                    Console.ReadKey();
+                }
+                Console.WriteLine(allLines[i]);
+            }
+            */
+
+            foreach (var line in allLines)
+            {
+                if(line.Contains("Chapter") == true)
+                {
+                    Console.ReadKey();
+                }
+                Console.WriteLine(line);
+            }
+
+        }
+    }
+}
